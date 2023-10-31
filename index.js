6bd05054e4e8186b8436817ff2a48f441e335d88
Collin <cdchamberlin.rva@gmail.com>
2023-10-30 20:37:56 -0400

    Finished Question 5. Had to remember that to create the ID for setTimeout, had to give it a reference point

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ let counter2 = makeCounter(1)
 counter1(); // 2
 counter1(); // 4
 counter2(); // 2
+
+// Question 2
+
+/*  function delayMsg(msg)
+{
+console.log(`This message will be printed after a delay: ${msg}`)
+} 
+ */
+const delayMsg = (msg) => console.log(`This message will be printed after a delay: ${msg}`)
+setTimeout(delayMsg, 100, '#1: Delayed by 100ms');
+setTimeout(delayMsg, 20, '#2: Delayed by 20ms');
+setTimeout(delayMsg, 0, '#3: Delayed by 0ms');
+delayMsg('#4: Not delayed at all')
+const m5 = setTimeout(delayMsg, 1e4, `#5 delayed by 10 seconds`)
+clearTimeout(m5)
