0860e24150018e8fe437b912c89305a3c5096714
Collin <cdchamberlin.rva@gmail.com>
2023-11-04 14:01:40 -0400

    initial code

--- index.js
+++ index.js
@@ -122,11 +122,43 @@ function Person(name, age, gender) {
   this.name = name;
   this.age = age;
   this.gender = gender;
-  Object.prototype.toString = () =>{ return `I'm ${this.name}, a ${this.age} old ${this.gender}`;}
+  Object.prototype.toString = () => {
+    return `I'm ${this.name}, a ${this.age} old ${this.gender}`;
+  };
 }
-  const person1 = new Person('James Brown', 73, 'male')
+/*  const person1 = new Person('James Brown', 73, 'male')
   console.log('person1: '+person1) //prints person1: [object Object]
 const person2 = new Person('Jimmy', 12, 'male')
 //const person4 = new Person('Cindy', 13, 'female')
 console.log('person 2: '+person2)
-console.log('person 1: '+person1)
+console.log('person 1: '+person1) */
+
+// Question 8
+// given
+class DigitalClock {
+  constructor(prefix) {
+    this.prefix = prefix;
+  }
+  display() {
+    let date = new Date();
+    //create 3 variables in one go using array destructuring
+    let [hours, mins, secs] = [
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+    ];
+    if (hours < 10) hours = "0" + hours;
+    if (mins < 10) mins = "0" + mins;
+    if (secs < 10) secs = "0" + secs;
+    console.log(`${this.prefix} ${hours}:${mins}:${secs}`);
+  }
+  stop() {
+    clearInterval(this.timer);
+  }
+  start() {
+    this.display();
+    this.timer = setInterval(() => this.display(), 1000);
+  }
+}
+const myClock = new DigitalClock("my clock:");
+myClock.start();
